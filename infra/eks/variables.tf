ad62f835ecea07cd7708488cd12339d9abcfde8b
surabhi sahay <surabhi.sahay03@gmail.com>
2025-11-25 22:10:09 -0500

    Changing the cluster name

--- infra/eks/variables.tf
+++ infra/eks/variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "The name for the EKS cluster."
   type        = string
-  default     = "cloudguard-eks" # <-- The default value
+  default     = "cloudguard-eks-v2" # <-- The default value
 }
 
 variable "cluster_version" {
